7ffd9be7f6f2fa11b176e9d72eb4bd10656ff53e
Willem Aandewiel <Willem@Aandewiel.nl>
2026-03-28 16:09:02 +0100

    Test tag-release.yml

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*** Last Changed: 2026-03-28 - 15:57 ***/
+/*** Last Changed: 2026-03-28 - 16:09 ***/
 #include <Arduino.h>
 #include <Wire.h>
 #include <lvgl.h>
@@ -13,7 +13,7 @@
 #include "sensorReader.h"
 #include "WiFiManagerExt.h"
 
-const char* PROG_VERSION = "v1.0.2";
+const char* PROG_VERSION = "v1.0.3";
 
 //--- Global objects
 static DashboardUi dashboardUi;
